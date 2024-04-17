14d1c72447ba7bb112f5fe792524d427c125ff6e
delmesia <wendellsmesia@gmail.com>
2024-04-17 17:25:31 +0800

    refactored input validation

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"delsanchez.gl/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     // Unique integer ID for each movie
@@ -11,4 +15,21 @@ type Movie struct {
 	Runtime Runtime  // Movie runtime/duration (in minutes)
 	Genres  []string // Slices of genres for the movie (romance, comedy, etc)
 	Version int32    // The version number starts at 1, and will be incremented each time the movie information is updated
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater that 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
